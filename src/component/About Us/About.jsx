c6d713441134b267af008f18d9331024f795197e
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2025-07-28 18:41:30 +0530

    about us page

--- src/component/About Us/About.jsx
+++ src/component/About Us/About.jsx
@@ -1,20 +1,62 @@
 import React from "react";
 import img from "../../assets/aboutus.jpg";
 import profile from "../../assets/profile.png";
-
+import img1 from "../../assets/about1.jpg";
+import img2 from "../../assets/about2.jpg";
+import addres from "../../assets/addres.png";
+import call from "../../assets/call.png";
+import mail from "../../assets/mail.png";
+import Button from "../Home/Button";
 const About = () => {
   return (
     <div>
       <div className="flex w-full justify-center items-center mt-10">
-        <p className="w-[526px] h-[366px] rotate-0 opacity-100 font-alexandria font-semibold text-[50px] leading-[100%] tracking-[2%] text-center">
-            “Every student, regardless of their background, deserves a real chance at success.”
+        <p className="w-[526px] h-[366px] rotate-0 opacity-100 font-alexandria font-bold text-[50px] leading-[100%] tracking-[2%] text-center">
+          “Every student, regardless of their background, deserves a real chance
+          at success.”
         </p>
       </div>
-      <div className="flex justify-between">
-        <div><p className="font-alexandria font-normal text-[72.36px] leading-[100%] tracking-[0%] text-center">The EpochFolio, 
-we are not just tackling the problem of poor placements — we’re redefining what’s possible for millions of students who’ve been overlooked, underserved, and underestimated by the traditional system.</p></div>
-        <div></div>
+      <div className="flex justify-between h-[412px] mx-20">
+        <div
+          className="flex-1 flex justify-center items-center"
+          style={{
+            backgroundImage: `url(${img2})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="flex-1 flex justify-center items-center h-full w-full bg-[#2104044f]">
+            <p className="font-alexandria text-white font-bold text-[24px] leading-[100%] tracking-[0%] text-center w-[464px] h-[257px] rotate-0 opacity-100 top-[64px] left-[45px]">
+              <span className="text-bold text-[73px]">T</span>
+              he EpochFolio, we are not just tackling the problem of poor
+              placements — we’re redefining what’s possible for millions of
+              students who’ve been overlooked, underserved, and underestimated
+              by the traditional system.
+            </p>
+          </div>
+        </div>
+
+        <div
+          className="flex-1 flex flex-col justify-center items-center"
+          style={{
+            backgroundImage: `url(${img1})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="flex-1 flex flex-col justify-center items-center h-full w-full bg-[#ffffff4f]">
+            <p className="font-alexandria text-[30px] font-bold leading-[100%] tracking-[0%] text-center">
+              Driven by Purpose
+            </p>
+            <p className="font-alexandria text-[30px] font-bold leading-[100%] tracking-[0%] text-center mt-5 text-[#8F1015]">
+              Built on Impact
+            </p>
+          </div>
+        </div>
       </div>
+
       <div className="flex flex-col lg:flex-row justify-between items-center px-10 lg:px-20 py-20 gap-10">
         <div className="flex-1 flex flex-col items-center gap-20">
           <div className="flex-1 flex flex-col items-center gap-10">
@@ -110,6 +152,105 @@ we are not just tackling the problem of poor placements — we’re redefining w
             <span className="text-[#E94D65]">— Naitik Joshi</span>, Founder,
             EpochFolio
           </p>
+        </div>
+      </div>
+
+      {/* contact us */}
+      <div className="flex mx-20 my-10 justify-center items-center h-[600px] gap-40">
+        <div className="flex flex-col items-center justify-center gap-15 w-1/2">
+          <div className="flex items-center justify-center gap-5">
+            {" "}
+            <div className="w-[59.20433044433594px] h-0 top-[79.73px] left-[197.19px] rotate-0 opacity-100 border-[2.86px]"></div>
+            <p className="font-inter font-semibold text-[15px] leading-[100%] tracking-[0%]">
+              CONTACT US WITH EASE
+            </p>
+            <div className="w-[59.20433044433594px] h-0 top-[79.73px] left-[197.19px] rotate-0 opacity-100 border-[2.86px]"></div>
+          </div>
+          <h1 className="font-inter font-semibold text-[46.26px] leading-[100%] tracking-[0%]">
+            {" "}
+            Get in <span className="text-[#FF9761]">touch</span>{" "}
+          </h1>
+          <p className="font-inter font-medium text-[14.04px] leading-[100%] tracking-[0%] text-center">
+            visit our agency or simply send us an email anytime you want.if you
+            have any questions,please feel free to contact us.
+          </p>
+          <div className="flex items-start justify-center gap-5">
+            <div className="flex-1 flex flex-col items-start justify-center gap-5 ">
+              <img
+                src={addres}
+                alt=""
+                width={78}
+                height={78}
+                className="w-[78.30250549316406px] h-[78.30250549316406px] top-[327.53px] left-[118.41px] rotate-0 opacity-100 object-bottom"
+              />
+              <h1 className="font-inter font-bold text-[17.19px] leading-[100%] tracking-[0%]">
+                Address
+              </h1>{" "}
+              <p className="font-inter font-normal text-[9.77px] leading-[100%] tracking-[0%]">
+                EpochFolio HQ, Pataudi, India
+              </p>
+            </div>
+            <div className="flex-1 flex flex-col items-start justify-center gap-5">
+              {" "}
+              <img
+                src={call}
+                width={78}
+                height={78}
+                alt=""
+                className="w-[78.30250549316406px] h-[78.30250549316406px] top-[327.53px] left-[118.41px] rotate-0 opacity-100 object-cover"
+              />
+              <h1 className="font-inter font-bold text-[17.19px] leading-[100%] tracking-[0%]">
+                Call Us
+              </h1>{" "}
+              <p className="font-inter font-normal text-[9.77px] leading-[100%] tracking-[0%]">
+                9016731145
+              </p>
+            </div>
+            <div className="flex-1 flex flex-col items-start justify-center gap-5">
+              {" "}
+              <img
+                src={mail}
+                width={78}
+                height={78}
+                alt=""
+                className="w-[78.30250549316406px] h-[78.30250549316406px] top-[327.53px] left-[118.41px] rotate-0 opacity-100 object-cover"
+              />
+              <h1 className="font-inter font-bold text-[17.19px] leading-[100%] tracking-[0%]">
+                Mail Us
+              </h1>{" "}
+              <p className="font-inter font-normal text-[9.77px] leading-[100%] tracking-[0%]">
+                contact@epochfolio.com
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-5 w-1/2">
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="w-full h-[43px] top-[109.81px] left-[633.1px] rotate-0 opacity-100 rounded-[4.77px] border-[0.95px] px-3"
+          />
+          <input
+            type="text"
+            placeholder="Your Email"
+            className="w-full h-[43px] top-[109.81px] left-[633.1px] rotate-0 opacity-100 rounded-[4.77px] border-[0.95px] px-3"
+          />
+          <input
+            type="text"
+            placeholder="Your Subject"
+            className="w-full h-[43px] top-[109.81px] left-[633.1px] rotate-0 opacity-100 rounded-[4.77px] border-[0.95px] px-3"
+          />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Your Message"
+            className="w-full h-[101.22030639648438px] top-[308.43px] left-[633.1px] rotate-0 opacity-100 rounded-[4.77px] border-[0.95px] px-3"
+          ></textarea>
+          <div className="flex w-full">
+            <Button text={"Send Message"} color={"bg-black text-white"} />
+          </div>
         </div>
       </div>
     </div>
